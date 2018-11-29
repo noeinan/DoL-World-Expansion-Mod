7b04acdecbbd41f021423cb91458f079a0d5b3f9
Vrelnir <vrelnir@gmail.com>
2018-11-29 00:12:11 +0000

    Moved base-clothing and globals content into base.js.

--- game/base.js
+++ game/base.js
@@ -157,11 +157,82 @@ Macro.add('time', {
 
 
 
+function DefineMacro(macroName, macroFunction) {
+	Macro.add(macroName, { isWidget: true, handler: function() {
+		var oldArgs = State.variables.args;
+		State.variables.args = this.args.slice();
+		macroFunction.apply(this, this.args);
+		if (typeof oldArgs === 'undefined') {
+			delete State.variables.args;
+		} else {
+			State.variables.args = oldArgs;
+		}
+	} });
+}
+
+/**
+ * Define macro, where macroFunction returns text to wikify & print
+ */
+function DefineMacroS(macroName, macroFunction) {
+	DefineMacro(macroName, function() {
+		$(this.output).wiki(macroFunction.apply(null,this.args))
+	});
+}
 
 
 
 
 
+function underintegrity(){
+	var output='';
+	var V = State.variables;
+	if (V.underintegritymax !== 0) {
+		if (V.underintegrity <= (V.underintegritymax / 10) * 2) {
+			output += "tattered \t";
+		} else if (V.underintegrity <= (V.underintegritymax / 10) * 5) {
+			output += "torn \t";
+		} else if (V.underintegrity <= (V.underintegritymax / 10) * 9) {
+			output += "frayed \t";
+		} else {
+		}
+	}
+	return output;
+}
+DefineMacroS("underintegrity", underintegrity);
+
+function lowerintegrity(){
+	var output='';
+	var V = State.variables;
+	if (V.lowerintegritymax !== 0) {
+		if (V.lowerintegrity <= (V.lowerintegritymax / 10) * 2) {
+			output += "tattered \t";
+		} else if (V.lowerintegrity <= (V.lowerintegritymax / 10) * 5) {
+			output += "torn \t";
+		} else if (V.lowerintegrity <= (V.lowerintegritymax / 10) * 9) {
+			output += "frayed \t";
+		} else {
+		}
+	}
+	return output;
+}
+DefineMacroS("lowerintegrity", lowerintegrity);
+
+function upperintegrity(){
+	var output='';
+	var V = State.variables;
+	if (V.upperintegritymax !== 0) {
+		if (V.upperintegrity <= (V.upperintegritymax / 10) * 2) {
+			output += "tattered \t";
+		} else if (V.upperintegrity <= (V.upperintegritymax / 10) * 5) {
+			output += "torn \t";
+		} else if (V.upperintegrity <= (V.upperintegritymax / 10) * 9) {
+			output += "frayed \t";
+		} else {
+		}
+	}
+	return output;
+}
+DefineMacroS("upperintegrity", upperintegrity);
 
 
 
