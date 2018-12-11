8d3102245312a97bee1d5e349aa4685b5bb292dc
aimozg <aimozg@ya.ru>
2018-12-12 00:07:42 +0600

    fixes

--- game/base.js
+++ game/base.js
@@ -53,7 +53,7 @@ Mousetrap.prototype.stopCallback = function(e, element, combo) {
 
 
 Mousetrap.bind(["z", "n", "enter"], function () {
-	$("#passages #next a.macro-click").trigger("click");
+	$("#passages #next a.macro-link").trigger("click");
 });
 
 
