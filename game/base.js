38c3eaa1407d362ef596df2fef5767a17aae726a
klorpa <30924131+klorpa@users.noreply.github.com>
2019-01-27 00:15:22 -0600

    Minor Fixes

--- game/base.js
+++ game/base.js
@@ -194,81 +194,81 @@ window.AvsAn = (function () {
 var enableLinkNumberify = true; // toggle this if you don't want to have the feature
  
 if (enableLinkNumberify) {
-    var disableNumberifyInVisibleElements = [
-        '#passage-hairdressers-seat',
-        '#passage-start',
-        '#passage-wardrobe',
+	var disableNumberifyInVisibleElements = [
+		'#passage-hairdressers-seat',
+		'#passage-start',
+		'#passage-wardrobe',
 		'#passage-cheats',
 		'#passage-testing-room'
-    ];
+	];
  
 // Number-ify links
-    var currentLinks = [];
+	var currentLinks = [];
  
-    function getPrettyKeyNumber(counter) {
-        var str = "";
+	function getPrettyKeyNumber(counter) {
+		var str = "";
  
-        if (counter > 30)
-            str = "Ctrl + ";
-        else if (counter > 20)
-            str = "Alt + ";
-        else if (counter > 10)
-            str = "Shift + ";
+		if (counter > 30)
+			str = "Ctrl + ";
+		else if (counter > 20)
+			str = "Alt + ";
+		else if (counter > 10)
+			str = "Shift + ";
  
-        if (counter % 10 === 0)
-            str += "0";
-        else if (counter < 10)
-            str += counter;
-        else {
-            var c = Math.floor(counter / 10);
-            str += (counter - (10 * c)).toString();
-        }
+		if (counter % 10 === 0)
+			str += "0";
+		else if (counter < 10)
+			str += counter;
+		else {
+			var c = Math.floor(counter / 10);
+			str += (counter - (10 * c)).toString();
+		}
  
-        return str;
-    }
+		return str;
+	}
  
-    $(document).on(':passagerender', function(ev) {
-        currentLinks = [];
+	$(document).on(':passagerender', function(ev) {
+		currentLinks = [];
  
-        for (var i = 0; i < disableNumberifyInVisibleElements.length; i++) {
-            if ($(ev.content).find(disableNumberifyInVisibleElements[i]).length || $(ev.content).is(disableNumberifyInVisibleElements[i]))
-                return; // simply skip this render
-        }
+		for (var i = 0; i < disableNumberifyInVisibleElements.length; i++) {
+			if ($(ev.content).find(disableNumberifyInVisibleElements[i]).length || $(ev.content).is(disableNumberifyInVisibleElements[i]))
+				return; // simply skip this render
+		}
  
-        currentLinks = $(ev.content).find(".link-internal"); // wanted to use .macro-link, but wardrobe and something else doesn't get selected, lmao
+		currentLinks = $(ev.content).find(".link-internal"); // wanted to use .macro-link, but wardrobe and something else doesn't get selected, lmao
  
-        $(currentLinks).each(function(i, el) {
-            $(el).html("(" + getPrettyKeyNumber(i + 1) + ") " + $(el).html());
-        });
-    });
+		$(currentLinks).each(function(i, el) {
+			$(el).html("(" + getPrettyKeyNumber(i + 1) + ") " + $(el).html());
+		});
+	});
  
-    $(document).on('keyup', function(ev) {
-        if ((ev.keyCode >= 48 && ev.keyCode <= 57) || (ev.keyCode >= 96 && ev.keyCode <= 105)) {
-            var fixedKeyIndex = (ev.keyCode < 60 ? ev.keyCode - 48 : ev.keyCode - 96);
+	$(document).on('keyup', function(ev) {
+		if ((ev.keyCode >= 48 && ev.keyCode <= 57) || (ev.keyCode >= 96 && ev.keyCode <= 105)) {
+			var fixedKeyIndex = (ev.keyCode < 60 ? ev.keyCode - 48 : ev.keyCode - 96);
  
-            var requestedLinkIndex = [
-                9,
-                0,
-                1,
-                2,
-                3,
-                4,
-                5,
-                6,
-                7,
-                8
-            ][fixedKeyIndex];
+			var requestedLinkIndex = [
+				9,
+				0,
+				1,
+				2,
+				3,
+				4,
+				5,
+				6,
+				7,
+				8
+			][fixedKeyIndex];
  
-            if (ev.ctrlKey)
-                requestedLinkIndex += 30;
-            else if (ev.altKey)
-                requestedLinkIndex += 20;
-            else if (ev.shiftKey)
-                requestedLinkIndex += 10;
+			if (ev.ctrlKey)
+				requestedLinkIndex += 30;
+			else if (ev.altKey)
+				requestedLinkIndex += 20;
+			else if (ev.shiftKey)
+				requestedLinkIndex += 10;
  
-            if ($(currentLinks).length >= requestedLinkIndex + 1)
-                $(currentLinks[requestedLinkIndex]).click();
-        }
-    });
+			if ($(currentLinks).length >= requestedLinkIndex + 1)
+				$(currentLinks[requestedLinkIndex]).click();
+		}
+	});
 }
 
