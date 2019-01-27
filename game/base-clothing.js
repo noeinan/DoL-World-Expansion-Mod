1debaf4a82a9b9308d1a521da8ab9b8da1f4e923
klorpa <30924131+klorpa@users.noreply.github.com>
2019-01-26 23:33:32 -0600

    Spaces

--- game/base-clothing.js
+++ game/base-clothing.js
@@ -1,9 +1,9 @@
 function colourContainerClasses() {
-	var V = State.variables;
-	return 'hair-'  + (V.haircolour||'').replace(/ /g,'-') +
-		' ' + 'eye-'   + (V.eyecolour||'').replace(/ /g,'-') +
-		' ' + 'upper-' + (V.uppercolour||'').replace(/ /g,'-') +
-		' ' + 'lower-' + (V.lowercolour||'').replace(/ /g,'-') +
-		' ' + 'under-' + (V.undercolour||'').replace(/ /g,'-')
+var V = State.variables;
+return 'hair-'  + (V.haircolour||'').replace(/ /g,'-') +
+' ' + 'eye-'   + (V.eyecolour||'').replace(/ /g,'-') +
+' ' + 'upper-' + (V.uppercolour||'').replace(/ /g,'-') +
+' ' + 'lower-' + (V.lowercolour||'').replace(/ /g,'-') +
+' ' + 'under-' + (V.undercolour||'').replace(/ /g,'-')
 }
 window.colourContainerClasses = colourContainerClasses; // export function
