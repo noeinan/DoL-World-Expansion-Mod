7c6e37aa4144b30e25ce26b2f86610dc81b47b03
Vrelnir <vrelnir@gmail.com>
2019-02-10 18:07:04 +0000

    Updated to 0.1.26.0

--- game/base.js
+++ game/base.js
@@ -19,6 +19,14 @@ prehistory['version-update'] = function () {
 	}
 };
 
+config.saves.autosave = "autosave";
+config.saves.isAllowed = function () {
+	if (tags().contains("nosave")) {
+		return false;
+	}
+	return true;
+};
+
 (function (window, define, exports) {
 
 /* mousetrap v1.6.2 craig.is/killing/mice */
@@ -198,89 +206,93 @@ window.AvsAn = (function () {
  * @type {boolean}
  */
 var enableLinkNumberify = true;
-
-if (enableLinkNumberify) {
-	var disableNumberifyInVisibleElements = [
-		'#passage-hairdressers-seat',
-		'#passage-start',
-		'#passage-wardrobe',
-		'#passage-cheats',
-		'#passage-testing-room'
-	];
-
-	// Number-ify links
-	var currentLinks = [];
-
-	function getPrettyKeyNumber(counter) {
-		var str = "";
-
-		if (counter > 30)
-			str = "Ctrl + ";
-		else if (counter > 20)
-			str = "Alt + ";
-		else if (counter > 10)
-			str = "Shift + ";
-
-		if (counter % 10 === 0)
-			str += "0";
-		else if (counter < 10)
-			str += counter;
-		else {
-			var c = Math.floor(counter / 10);
-			str += (counter - (10 * c)).toString();
-		}
-
-		return str;
-	}
-
-	$(document).on(':passagerender', function(ev) {
-		currentLinks = [];
-
-		if (!State.variables.numberify_enabled)
-			return;
-
-		for (var i = 0; i < disableNumberifyInVisibleElements.length; i++) {
-			if ($(ev.content).find(disableNumberifyInVisibleElements[i]).length || $(ev.content).is(disableNumberifyInVisibleElements[i]))
-				return; // simply skip this render
-		}
-
-		currentLinks = $(ev.content).find(".link-internal"); // wanted to use .macro-link, but wardrobe and something else doesn't get selected, lmao
-
-		$(currentLinks).each(function(i, el) {
-			$(el).html("(" + getPrettyKeyNumber(i + 1) + ") " + $(el).html());
-		});
-	});
-
-	$(document).on('keyup', function(ev) {
-		if (!State.variables.numberify_enabled)
-			return;
-
-		if ((ev.keyCode >= 48 && ev.keyCode <= 57) || (ev.keyCode >= 96 && ev.keyCode <= 105)) {
-			var fixedKeyIndex = (ev.keyCode < 60 ? ev.keyCode - 48 : ev.keyCode - 96);
-
-			var requestedLinkIndex = [
-				9,
-				0,
-				1,
-				2,
-				3,
-				4,
-				5,
-				6,
-				7,
-				8
-			][fixedKeyIndex];
-
-			if (ev.ctrlKey)
-				requestedLinkIndex += 30;
-			else if (ev.altKey)
-				requestedLinkIndex += 20;
-			else if (ev.shiftKey)
-				requestedLinkIndex += 10;
-
-			if ($(currentLinks).length >= requestedLinkIndex + 1)
-				$(currentLinks[requestedLinkIndex]).click();
-		}
-	});
+ 
+var disableNumberifyInVisibleElements = [
+    '#passage-hairdressers-seat',
+    '#passage-start',
+    '#passage-wardrobe',
+    '#passage-cheats',
+	'#passage-changing-room',
+	'#passage-eden-wardrobe',
+	'#passage-asylum-wardrobe',
+	'#passage-strip-club-dressing-room',
+	'#passage-brothel-dressing-room',
+    '#passage-testing-room'
+];
+ 
+// Number-ify links
+var currentLinks = [];
+ 
+function getPrettyKeyNumber(counter) {
+    var str = "";
+ 
+    if (counter > 30)
+        str = "Ctrl + ";
+    else if (counter > 20)
+        str = "Alt + ";
+    else if (counter > 10)
+        str = "Shift + ";
+ 
+    if (counter % 10 === 0)
+        str += "0";
+    else if (counter < 10)
+        str += counter;
+    else {
+        var c = Math.floor(counter / 10);
+        str += (counter - (10 * c)).toString();
+    }
+ 
+    return str;
 }
+ 
+$(document).on(':passagerender', function(ev) {
+    currentLinks = [];
+ 
+    if (!State.variables.numberify_enabled || !enableLinkNumberify)
+        return;
+ 
+    for (var i = 0; i < disableNumberifyInVisibleElements.length; i++) {
+        if ($(ev.content).find(disableNumberifyInVisibleElements[i]).length || $(ev.content).is(disableNumberifyInVisibleElements[i]))
+            return; // simply skip this render
+    }
+ 
+    currentLinks = $(ev.content).find(".link-internal"); // wanted to use .macro-link, but wardrobe and something else doesn't get selected, lmao
+ 
+    $(currentLinks).each(function(i, el) {
+        $(el).html("(" + getPrettyKeyNumber(i + 1) + ") " + $(el).html());
+    });
+});
+ 
+$(document).on('keyup', function(ev) {
+    if (!State.variables.numberify_enabled || !enableLinkNumberify)
+        return;
+ 
+    if ((ev.keyCode >= 48 && ev.keyCode <= 57) || (ev.keyCode >= 96 && ev.keyCode <= 105)) {
+        var fixedKeyIndex = (ev.keyCode < 60 ? ev.keyCode - 48 : ev.keyCode - 96);
+ 
+        var requestedLinkIndex = [
+            9,
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8
+        ][fixedKeyIndex];
+ 
+        if (ev.ctrlKey)
+            requestedLinkIndex += 30;
+        else if (ev.altKey)
+            requestedLinkIndex += 20;
+        else if (ev.shiftKey)
+            requestedLinkIndex += 10;
+ 
+        if ($(currentLinks).length >= requestedLinkIndex + 1)
+            $(currentLinks[requestedLinkIndex]).click();
+    }
+});
+
 
