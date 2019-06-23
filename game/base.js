626921d4470729ddce6f8f880192624a5b477272
klorpa <30924131+klorpa@users.noreply.github.com>
2019-06-23 18:38:09 -0500

    Spaces

--- game/base.js
+++ game/base.js
@@ -4,30 +4,30 @@ Config.history.maxStates = 20;
 
 State.initPRNG();
 
-document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchstart', handleTouchStart, false);
 document.addEventListener('touchmove', handleTouchMove, false);
 
-var xDown = null;                                                        
+var xDown = null;
 var yDown = null;
 
 /*Sidebar swipe*/
 function getTouches(evt) {
   return evt.touches ||             // browser API
          evt.originalEvent.touches; // jQuery
-}                                                     
+}
 
 function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
 
 function handleTouchMove(evt) {
     if ( ! xDown || ! yDown ) {
         return;
     }
 
-    var xUp = evt.touches[0].clientX;                                    
+    var xUp = evt.touches[0].clientX;
     var yUp = evt.touches[0].clientY;
 
     var xDiff = xDown - xUp;
@@ -35,20 +35,20 @@ function handleTouchMove(evt) {
 
     if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
         if ( xDiff > 0 ) {
-           UIBar.stow();/* left swipe */ 
+           UIBar.stow();/* left swipe */
         } else {
            UIBar.unstow();/* right swipe */
-        }                       
+        }
     } else {
         if ( yDiff > 0 ) {
-            /* up swipe */ 
-        } else { 
+            /* up swipe */
+        } else {
             /* down swipe */
-        }                                                                 
+        }
     }
     /* reset values */
     xDown = null;
-    yDown = null;                                             
+    yDown = null;
 };
 
 
