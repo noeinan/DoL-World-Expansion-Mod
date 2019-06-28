33a4ce4eea4dc1295b3888eca5b15cb2ec605d4c
Vrelnir <vrelnir@gmail.com>
2019-06-28 11:15:28 +0100

    Updated to 0.2.0.2

--- game/base.js
+++ game/base.js
@@ -1,10 +1,12 @@
 config.history.controls = false;
 
-Config.history.maxStates = 20;
+Config.history.maxStates = 2;
 
 State.initPRNG();
 
-document.addEventListener('touchstart', handleTouchStart, false);
+
+
+document.addEventListener('touchstart', handleTouchStart, false);        
 document.addEventListener('touchmove', handleTouchMove, false);
 
 window.SerializeGame = function () { return Save.serialize(); }; window.DeserializeGame = function (myGameState) { return Save.deserialize(myGameState) };
@@ -16,7 +18,7 @@ var yDown = null;
 function getTouches(evt) {
   return evt.touches ||             // browser API
          evt.originalEvent.touches; // jQuery
-}
+}                                                     
 
 function handleTouchStart(evt) {
     var firstTouch = getTouches(evt)[0];                                      
@@ -29,7 +31,7 @@ function handleTouchMove(evt) {
         return;
     }
 
-    var xUp = evt.touches[0].clientX;
+    var xUp = evt.touches[0].clientX;                                    
     var yUp = evt.touches[0].clientY;
 
     var xDiff = xDown - xUp;
@@ -37,20 +39,20 @@ function handleTouchMove(evt) {
 
     if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
         if ( xDiff > 0 ) {
-           UIBar.stow();/* left swipe */
+           UIBar.stow();/* left swipe */ 
         } else {
            UIBar.unstow();/* right swipe */
-        }
+        }                       
     } else {
         if ( yDiff > 0 ) {
-            /* up swipe */
-        } else {
+            /* up swipe */ 
+        } else { 
             /* down swipe */
-        }
+        }                                                                 
     }
     /* reset values */
     xDown = null;
-    yDown = null;
+    yDown = null;                                             
 };
 
 
