77807164140dc9dc76442f5c5fe176b7d64c8ea9
klorpa <30924131+klorpa@users.noreply.github.com>
2019-08-30 22:07:09 -0500

    RenameAndSet

--- game/base-system/pronouns.js
+++ game/base-system/pronouns.js
@@ -4,49 +4,37 @@ App.Utils.Pronouns = class {
 		this._pronouns = npc;
 	}
 
-	get pronoun() { return this._pronouns.pronoun; }
-	get possessivePronoun() { return this._pronouns.possessivePronoun; }
-	get possessive() { return this._pronouns.possessive; }
-	get object() { return this._pronouns.object; }
-	get objectReflexive() { return this._pronouns.objectReflexive; }
-	get noun() { return this._pronouns.noun; }
+	get he() { return this._pronouns.he; }
+	get hers() { return this._pronouns.hers; }
+	get his() { return this._pronouns.his; }
+	get him() { return this._pronouns.him; }
+	get himself() { return this._pronouns.himself; }
+	get boy() { return this._pronouns.boy; }
 
-	get Pronoun() { return capFirstChar(this.pronoun); }
-	get PossessivePronoun() { return capFirstChar(this.possessivePronoun); }
-	get Possessive() { return capFirstChar(this.possessive); }
-	get Object() { return capFirstChar(this.object); }
-	get ObjectReflexive() { return capFirstChar(this.objectReflexive); }
-	get Noun() { return capFirstChar(this.noun); }
+	get He() { return capFirstChar(this.pronoun); }
+	get Hers() { return capFirstChar(this.hers); }
+	get His() { return capFirstChar(this.his); }
+	get Him() { return capFirstChar(this.him); }
+	get Himself() { return capFirstChar(this.himself); }
+	get Boy() { return capFirstChar(this.boy); }
 
-	get he() { return this.pronoun; }
-	get him() { return this.object; }
-	get his() { return this.possessive; }
-	get himself() { return this.objectReflexive; }
-	get boy() { return this.noun; }
-
-	get He() { return this.Pronoun; }
-	get Him() { return this.Object; }
-	get His() { return this.Possessive; }
-	get Himself() { return this.ObjectReflexive; }
-	get Boy() { return this.Noun; }
-
-	get man() { return this.noun === "girl" ? "woman" : "man"; }
-	get men() { return this.noun === "girl" ? "women" : "men"; }
-	get shota() { return this.noun === "girl" ? "loli" : "shota"; }
+	get man() { return this.boy === "girl" ? "woman" : "man"; }
+	get men() { return this.boy === "girl" ? "women" : "men"; }
+	get shota() { return this.boy === "girl" ? "loli" : "shota"; }
 
 	get Man() { return capFirstChar(this.man); }
 	get Men() { return capFirstChar(this.men); }
 	get Shota() { return capFirstChar(this.shota); }
 
-	get son() { return this.noun === "girl" ? "daughter" : "son"; }
+	get son() { return this.boy === "b" ? "daughter" : "son"; }
 	get Son() { return capFirstChar(this.son); }
 
-	get brother() { return this.noun === "girl" ? "sister" : "brother"; }
+	get brother() { return this.boy === "girl" ? "sister" : "brother"; }
 	get Brother() { return capFirstChar(this.brother); }
 
-	get husband() { return this.noun === "girl" ? "wife" : "husband"; }
+	get husband() { return this.boy === "girl" ? "wife" : "husband"; }
 	get Husband() { return capFirstChar(this.husband); }
-	get husbands() { return this.noun === "girl" ? "wives" : "husbands"; }
+	get husbands() { return this.boy === "girl" ? "wives" : "husbands"; }
 	get Husbands() { return capFirstChar(this.husbands); }
 
 };
