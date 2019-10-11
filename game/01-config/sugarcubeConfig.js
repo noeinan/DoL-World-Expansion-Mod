fbd4a0ea5fe3a8a719df4695b1567f1e08ad0a86
LollipopScythe <11764-LollipopScythe@users.noreply.gitgud.io>
2019-10-11 15:58:52 -0700

    Initial Folder/File Reorganization
    - Added folder '01-config' to store any files relating to the initiation of the game
    = Added new variable 'StartConfig' to centralize config variables that change between different versions of the game
    - Added folder '02-CSS' to store any CSS files to help organize the CSS
    - Added folder '03-JavaScript' to store all JavaScript Files
    - Initial separation of 'base.js' as certain git management tools are unable to read it
    - Added folder '04-Variable' to create a centralized place to store and manage them.
    - Initial setup of new files in '04-Variable' in preparation of moving relevant variables to them

---
+++ game/01-config/sugarcubeConfig.js
@@ -0,0 +1,48 @@
+Config.history.controls = false;
+
+Config.history.maxStates = 1;
+
+State.initPRNG();
+
+window.versionUpdateCheck = true;
+
+Config.saves.onLoad = function (save) {
+    window.versionUpdateCheck = true;
+}
+
+/*LinkNumberify and images will enable or disable the feature completely*/
+/*debug will enable or disable the feature only for new games*/
+window.StartConfig = {
+    "debug": true,
+    "enableImages": true,
+    "enableLinkNumberify": true,
+    "version": "0.2.4.2 Folder Change",
+}
+
+config.saves.autosave = "autosave";
+
+Config.saves.isAllowed = function () {
+	if (tags().contains("nosave")) {
+		return false;
+	}
+	return true;
+};
+
+prehistory['version-update'] = function () {
+	if (Story.has('VersionUpdate')) {
+		try {
+			Wikifier.wikifyEval(Story.get('VersionUpdate').text);
+		}
+		catch (ex) {
+			Alert.error('VersionUpdate', ex.message);
+		}
+	}
+};
+
+importStyles("style.css")
+.then(function () {
+	console.log("External Style Sheet Active")
+})
+.catch(function (err) {
+	console.log("External Style Sheet Missing");
+});
