2e17d97cd978627a69c567ceec31ba1547ad65e0
shikiyoku <14437-shikiyoku@users.noreply.gitgud.io>
2020-03-25 05:52:10 -0700

    Add favicon using tweego --head parameter
    - Put favicon HTML to `build/head.html` and pass the file to tweego to append it to `<head>`

--- compile watch.bat
+++ compile watch.bat
@@ -1,2 +1,2 @@
 @set TWEEGO_PATH=%cd%\StoryFormats
-@tweego -w -o "Degrees of Lewdity VERSION.html" game
+@tweego -w -o "Degrees of Lewdity VERSION.html" --head "build/head.html" game
--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
 @set TWEEGO_PATH=%cd%\StoryFormats
-@tweego -o "Degrees of Lewdity VERSION.html" game
+@tweego -o "Degrees of Lewdity VERSION.html" --head "build/head.html" game
