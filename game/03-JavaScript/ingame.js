0cab8575fc42ade51462650e1902fa543810ded0
LollipopScythe <11764-LollipopScythe@users.noreply.gitgud.io>
2020-04-10 15:00:59 -0700

    Initial conversion of combat to lists
    - Includes converting hand actions into a list

--- game/03-JavaScript/ingame.js
+++ game/03-JavaScript/ingame.js
@@ -55,3 +55,96 @@ window.wikifier = function(widget, arg1, arg2, arg3){
 		new Wikifier(null, '<<'+widget+'>>');
 	}
 }
+
+window.wikifier2 = function(str){
+	new Wikifier(null, str);
+}
+
+window.combatListColor = function(name, value){
+	var color = "";
+	var check = "";
+	if(value != undefined){
+		check = value;
+	}else{
+		check = SugarCube.State.variables[name];
+	}
+	
+	switch(check) {
+		/*leftaction or rightaction*/
+		case "steal": case "penwhack": case "leftcovervagina": case "leftcoverpenis": case "leftcoveranus": case "rightcovervagina":
+		case "rightcoverpenis": case "rightcoveranus":
+		/*mouthaction*/
+		case "pullaway": case "pullawayvagina": case "finish": case "novaginal": case "nopenile": case "noanal": case "scream":
+		case "mock": case "breastclosed": case "breastpull": case "pullawaykiss":
+		/*penisaction*/
+		case "othermouthescape": case "escape": case "otheranusescape":
+			color = "brat";
+		break;
+		
+		/*leftaction or rightaction*/
+		case "spray": case "lefthit": case "righthit": case "leftstruggle": case "rightstruggle":
+		/*feetaction*/
+		case "kick":
+		/*mouthaction*/
+		case "bite": case "demand":
+			color = "def";
+		break;
+
+		/*mouthaction*/
+		case "grasp": case "plead": case "forgive":
+		/*penisaction*/
+		case "thighbay": case "bay": case "otheranusbay":
+		/*vaginaaction*/
+		case "penisthighs":
+		/*anusaction*/
+		case "bottombay": case "penischeeks": case "penispussy":
+			color = "meek";
+		break;
+
+		/*leftaction or rightaction*/
+		case "leftplay": case "leftgrab": case "leftstroke": case "leftchest": case "rightplay": case "rightgrab": case "rightstroke": case "rightchest":
+		case "leftchest": case "rightchest": case "leftwork": case "rightwork": case "leftclit": case "rightclit":
+		/*feetaction*/
+		case "grab": case "vaginagrab": case "grabrub": case "vaginagrabrub": case "rub":
+		/*mouthaction*/
+		case "peniskiss": case "kiss": case "suck": case "lick": case "moan": case "breastsuck": case "breastlick": case "swallow": case "movetochest":
+		case "othervagina": case "mouth": case "kissback":
+		/*penisaction*/
+		case "penistovagina": case "penistoanus": case "penisvaginafuck": case "penisanusfuck": case "othermouthtease": case "othermouthrub":
+		case "othermouthcooperate": case "tease": case "cooperate": case "otheranustease": case "otheranusrub": case "otheranuscooperate": case "clitrub":
+		/*vaginaaction*/
+		case "vaginatopenis": case "vaginapenisfuck":
+		/*anusaction*/
+		case "anustopenis": case "anuspenisfuck": case "penistease":
+			color = "sub";
+		break;
+
+		default:
+			color = "white";
+		break;
+	}
+	return color;
+}
+
+DefineMacroS("combatListColor", combatListColor);
+
+function combatButtonAdjustments(name){
+	jQuery(document).on('change', '#listbox-'+name,{"name":name}, function(e) {
+		/*console.log(e.data);*/
+		new Wikifier(null, '<<replace #'+e.data.name+'Difficulty>><<'+e.data.name+'Difficulty>><</replace>>');
+		$('#'+e.data.name+'Select' ).removeClass();
+		$('#'+e.data.name+'Select' ).addClass(combatListColor(e.data.name) + "List");
+	});
+	return "";
+}
+
+DefineMacroS("combatButtonAdjustments", combatButtonAdjustments);
+
+function combatDefaults(){
+	jQuery(document).on('change', '#listbox--defaultactions', function(e) {
+		new Wikifier(null, '<<replace #othersFeelings>><<othersFeelings '+this.value+'>><</replace>>');
+	});
+	return "";
+}
+
+DefineMacroS("combatDefaults", combatDefaults);
