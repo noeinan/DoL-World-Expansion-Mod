0f5ed80f31a7e88d93978663105a63dc5c4c7cbc
Vrelnir <vrelnir@gmail.com>
2020-05-25 03:23:11 +0100

    Updated to 0.2.13.0

--- game/01-config/sugarcubeConfig.js
+++ game/01-config/sugarcubeConfig.js
@@ -20,7 +20,7 @@ window.StartConfig = {
 	"debug": false,
 	"enableImages": true,
 	"enableLinkNumberify": true,
-	"version": "0.2.12.5",
+	"version": "0.2.13.0",
 }
 
 config.saves.autosave = "autosave";
