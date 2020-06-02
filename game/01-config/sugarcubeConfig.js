d29bf303d3d0111437d830d453d57220e23e596e
LollipopScythe <11764-LollipopScythe@users.noreply.gitgud.io>
2020-06-02 13:09:29 -0700

    Added initial checks for preventing NPCs doing actions that would make them all potential contortionists

--- game/01-config/sugarcubeConfig.js
+++ game/01-config/sugarcubeConfig.js
@@ -20,7 +20,7 @@ window.StartConfig = {
 	"debug": false,
 	"enableImages": true,
 	"enableLinkNumberify": true,
-	"version": "0.2.13.4",
+	"version": "0.2.13.4_combat_debug",
 }
 
 config.saves.autosave = "autosave";
