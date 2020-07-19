a16d9f8b6b001e5eb077670d87e27c90d696449a
00c356 <00c356@gmail.com>
2020-07-18 21:18:49 -0700

    Additional cleanup and documentation

--- game/base-system/tools/dynamicRendering.js
+++ game/base-system/tools/dynamicRendering.js
@@ -1,4 +1,3 @@
-
 window.Dynamic = window.Dynamic || {};
 
 /**
@@ -10,19 +9,10 @@ Dynamic.render = () => {
 	});
 	Links.generate();
 }
+
 /** 
- * Underlying implementation of the <<dynamic>> widget. You probably want to use that instead.
- * Slightly more powerful in that any arbitrary sugarcube content is allowed.
+ * Dynamic.eventBinder
  * 
- * Usage: <div id="Dynamic.bind('Any sugarcube content can go here, and will be manually be evaluated. <<myDynamicContent>>')></div>" 
- */
-Dynamic.bind = (content, customId) => {
-	const id = customId || Dynamic.getNewId();
-	Dynamic.bindTo(id, content);
-	setTimeout(() => Dynamic.renderAt(id, content), 0);
-	return id;
-}
-/** 
  * Will cause events on the bound element to re-render dynamic elements on the current passage.
  * Also allows you to immediately evaluate SugarCube variables, and use those value in the event handler
  * (manual string concat is no longer required)
@@ -37,18 +27,69 @@ Dynamic.bind = (content, customId) => {
  * 	@id="Dynamic.eventBinder(_valueToBeInjected1, $anotherVar)" 
  * 	onclick="(injectedValue1, injectedValue2) => { $var1 = injectedValue1; $var2 = injectedValue2; }"
  * ></div>" 
+ * 
+ * As the eventBinder is slightly complex, let me explain the issues with *not* using a utility like eventBinder.
+ * 
+ * #### Without eventBinder 
+ * 
+ * Even in the simplest case, using something like `@onclick="$var=1; Dynamic.render()"` does not perform as expected (nor does the non-`@` version). In order to correctly reference `$var`, you must either refer to `SugarCube.State.variables.var` in the literal `onclick=` handler, OR build an explicit string version using `@onclick=`. For reference, the `@onclick=` version is as follows 
+ * ```@onclick="return 'SugarCube.State.variables.var=1; Dynamic.render()'"``` 
+ * 
+ * The reason one might need to use the `@onclick` version is, some variables (for example, temporary ones) may not be available or overwritten when the click is evaluated. For example, if instead we wanted `$var = _myName`, in order to express this, we'd have to do: 
+ * ```@onclick="return 'SugarCube.State.variables.var=\'' + _myName + '\'; Dynamic.render()'"``` 
+ * 
+ * This is both error prone and difficult to read (missing one of the `'` will result in an error, as will adding an extra `'`). 
+ * 
+ * #### With eventBinder 
+ * 
+ * With event binder, we allow users to naturally write events, without having to build strings. 
+ * 
+ * For the above example, we write: 
+ * ```onclick="(myName) => $var = myName"``` 
+ * Note that `Dynamic.render()` will automatically be invoked after handling the value. 
+ * 
+ * For those unfamiliar with ES6, we could alternatively write this as... 
+ * ```onclick="function doit(myName) { $var = myName; }"``` 
+ * 
+ * In order to correctly bind `myName` (the function argument) to `_myName` the SugarCube value, we use the `Dynamic.eventBinder` utility, which is used by assigning to the `id` attribute. 
+ * ```@id="Dynamic.eventBinder(_myName)"``` 
+ * 
+ * Note that multiple arguments may be passed. Their values will be supplied to the event handler in the same order. 
+ * ```@id="Dynamic.eventBinder(_var1, _var3, _var2)" onclick="(var1, var3, var2) => ..."```
  */
 Dynamic.eventBinder = (...valuesToInject) => {
 	const id = Dynamic.getNewId();
+	/** We must let SugarCube finish rendering. Our element won't exist yet */
 	setTimeout(() => {
 		const elm = document.getElementById(id);
-		Dynamic.bindEvents(elm, ...valuesToInject)
+		Dynamic.bindEvents(elm, {}, valuesToInject)
 	})
 	return id;
 }
 
-/** #####################################################
- * Dynamic tools (you probably don't need to use these) 
+/** 
+ * Underlying implementation of the <<dynamic>> widget. You probably want to use that instead.
+ * Slightly more powerful in that any arbitrary sugarcube content is allowed.
+ * 
+ * Usage: <div id="Dynamic.bind('Any sugarcube content can go here, and will be manually be evaluated. <<myDynamicContent>>')></div>" 
+ */
+Dynamic.bind = (content, customId) => {
+	const id = customId || Dynamic.getNewId();
+	Dynamic.bindTo(id, content);
+	setTimeout(() => Dynamic.renderAt(id, content), 0);
+	return id;
+}
+
+/** ################
+ * Dynamic lifecycle
+ */
+$(document).on(':passageinit', function (ev) {
+	// cleanup existing IDs and usages before we start rendering the next passage
+	Dynamic.dispose();
+});
+
+/** ############################################################
+ * Dynamic internal tools (you probably don't need to use these) 
  */
 
 Dynamic.nextGlobalId = 0
@@ -67,22 +108,33 @@ Dynamic.renderAt = (id, content) => {
 	}
 }
 const SUGARCUBE_IDENTIFIER = /(.*?)\$([A-Za-z0-9_]+)/g;
-Dynamic.bindEvents = (elm, ...valuesToInject) => {
-	if (elm.onclick) {
-		const handler = elm.onclick.toString();
-		let handlerContents = handler.slice(handler.search(/\{/g) + 1, handler.length - 1);
-		SUGARCUBE_IDENTIFIER.lastIndex = 0;
-		let lastMatch = SUGARCUBE_IDENTIFIER.exec(handlerContents);
-		while (lastMatch) {
-			const newContent = `SugarCube.State.variables.${lastMatch[2]}`;
-			handlerContents = lastMatch[1] + newContent
-				+ handlerContents.slice(SUGARCUBE_IDENTIFIER.lastIndex)
-			SUGARCUBE_IDENTIFIER.lastIndex = lastMatch.index + newContent.length;
-			lastMatch = SUGARCUBE_IDENTIFIER.exec(handlerContents);
+const LITERAL_EVENTS = Object.getOwnPropertyNames(window)
+	.filter(g => g.startsWith("HTML"))
+	.map(k => window[k])
+	.map(c => Object.getOwnPropertyNames(c.prototype).filter(p => p.startsWith("on")))
+	.reduce((acc, n) => new Set([...acc].concat(n)), [])
+Dynamic.bindEvents = (elm, options, valuesToInject) => {
+	const handlersToBind = options.customHandlers ? [...options.customHandlers, ...LITERAL_EVENTS] : LITERAL_EVENTS;
+	handlersToBind.forEach(handlerName => {
+		if (elm[handlerName]) {
+			elm[handlerName] = Dynamic.createBoundHandler(elm[handlerName], ...valuesToInject);
 		}
-		const newHandler = new Function(`return (${handlerContents})`)()
-		elm.onclick = () => {
-			newHandler(...valuesToInject); Dynamic.render();
-		}
+	});
+}
+Dynamic.createBoundHandler = (sourceHandler, ...valuesToInject) => {
+	const handlerText = sourceHandler.toString();
+	let handlerContents = handlerText.slice(handlerText.search(/\{/g) + 1, handlerText.length - 1);
+	SUGARCUBE_IDENTIFIER.lastIndex = 0;
+	let lastMatch = SUGARCUBE_IDENTIFIER.exec(handlerContents);
+	while (lastMatch) {
+		const newContent = `SugarCube.State.variables.${lastMatch[2]}`;
+		handlerContents = lastMatch[1] + newContent
+			+ handlerContents.slice(SUGARCUBE_IDENTIFIER.lastIndex)
+		SUGARCUBE_IDENTIFIER.lastIndex = lastMatch.index + newContent.length;
+		lastMatch = SUGARCUBE_IDENTIFIER.exec(handlerContents);
+	}
+	const newHandler = new Function(`return (${handlerContents})`)()
+	return () => {
+		newHandler(...valuesToInject); Dynamic.render();
 	}
 }
