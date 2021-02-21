d873595daae95b4025b5443b7db22f6b95f8f835
Vrelnir <vrelnir@gmail.com>
2021-02-21 15:04:15 +0000

    Updated to 0.2.22.5

--- game/01-config/sugarcubeConfig.js
+++ game/01-config/sugarcubeConfig.js
@@ -21,7 +21,7 @@ window.StartConfig = {
 	"debug": false,
 	"enableImages": true,
 	"enableLinkNumberify": true,
-	"version": "0.2.22.4",
+	"version": "0.2.22.5",
 }
 
 config.saves.autosave = "autosave";
--- game/03-JavaScript/ingame.js
+++ game/03-JavaScript/ingame.js
@@ -262,3 +262,22 @@ function startingPlayerImageReset() {
 }
 
 DefineMacroS("startingPlayerImageReset", startingPlayerImageReset);
+
+window.deck = function(){
+    var names = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    var suits = ['Hearts','Diamonds','Spades','Clubs'];
+    var cards = [];
+    
+    for( var s = 0; s < suits.length; s++ ) {
+        for( var n = 0; n < names.length; n++ ) {
+            cards.push( {value:n+2, name:names[n], suits:suits[s]} );
+        }
+    }
+
+    return cards;
+}
+
+window.shuffle = function(o) {
+    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
