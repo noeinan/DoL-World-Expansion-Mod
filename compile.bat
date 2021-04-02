ae890c4b2fad7e02fa730f73b82eaa09856f919a
noeinan <daikiraikimi@gmail.com>
2021-04-01 21:14:25 -0700

    Fixing Bugs in Whitney Walk Scene
    Buncha bugs from using wrong variables.

--- compile.bat
+++ compile.bat
@@ -6,7 +6,7 @@ pushd %~dp0
 
 :: Run the appropriate compiler for the user's CPU architecture.
 if %PROCESSOR_ARCHITECTURE% == AMD64 (
-	CALL "%~dp0devTools\tweego\tweego_win64.exe" -o "%~dp0Degrees of Lewdity VERSION.html" --head "%~dp0devTools\head.html" "%~dp0game"
+	CALL "%~dp0devTools\tweego\tweego_win64.exe" -o "%~dp0Degrees of Lewdity.html" --head "%~dp0devTools\head.html" "%~dp0game"
 ) else (
-	CALL "%~dp0devTools\tweego\tweego_win86.exe" -o "%~dp0Degrees of Lewdity VERSION.html" --head "%~dp0devTools\head.html" "%~dp0game"
+	CALL "%~dp0devTools\tweego\tweego_win86.exe" -o "%~dp0Degrees of Lewdity.html" --head "%~dp0devTools\head.html" "%~dp0game"
 )
